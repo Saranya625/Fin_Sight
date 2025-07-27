cc241c767ad07b92d6e325cd7c581173af9daffa
Saranya <cs23b031@iittp.ac.in>
2025-07-27 15:27:02 +0530

    Delete finance-backend directory

--- finance-backend/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getDashboardStats } from '../controllers/dashboardController.js';
-
-const router = express.Router();
-
-router.get('/:userId', getDashboardStats);
-
-export default router;
